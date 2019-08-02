254761b972dadd5a6603e3095c8ec8080421a8ac
yemai <node_github@163.com>
2019-08-02 11:07:05 +0800

    Feat: add test

---
+++ test/index.svelte
@@ -0,0 +1,33 @@
+<h1>Hello Lazy!</h1>
+
+<img alt="" src="https://picsum.photos/id/412/200/300" />
+<img alt="" src="https://picsum.photos/id/412/200/300" />
+<img alt="" src="https://picsum.photos/id/412/200/300" />
+
+{#each [0, 1, 2, 3, 4, 5] as i}
+  <Lazy height={400} offset={150} onload={onload}>
+    <img alt="" src="https://picsum.photos/id/547/200/300" />
+  </Lazy>
+{/each}
+{#each [0, 1, 2, 3, 4, 5] as i}
+  <Lazy height={400} offset={150} onload={onload}>
+    <div>Any content can be here.</div>
+  </Lazy>
+{/each}
+
+<script>
+  import Lazy from '../src/index.svelte';
+
+  const onload = (node) => {
+    console.log('on load');
+  }
+</script>
+
+<style>
+  img {
+    display: block;
+  }
+  div {
+    height: 300px;
+  }
+</style>
---
+++ test/rollup.config.js
@@ -0,0 +1,16 @@
+import svelte from 'rollup-plugin-svelte';
+import resolve from 'rollup-plugin-node-resolve';
+import livereload from 'rollup-plugin-livereload';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'test/main.js',
+  output: [
+    { file: 'test/index.js', format: 'iife', name: 'app' },
+  ],
+  plugins: [
+    svelte(),
+    resolve({ browser: true }),
+  ],
+};
