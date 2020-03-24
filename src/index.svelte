e48405accb0ef490554ef99b75a3f9896aabc740
leafOfTree <node_github@163.com>
2020-03-24 17:45:55 +0800

    Replace tabs with spaces

--- src/index.svelte
+++ src/index.svelte
@@ -65,36 +65,36 @@
   }
 
   // from underscore souce code
-	function throttle(func, wait, options) {
-		let context, args, result;
-		let timeout = null;
-		let previous = 0;
-		if (!options) options = {};
-		const later = function() {
-			previous = options.leading === false ? 0 : new Date();
-			timeout = null;
-			result = func.apply(context, args);
-			if (!timeout) context = args = null;
-		};
+  function throttle(func, wait, options) {
+    let context, args, result;
+    let timeout = null;
+    let previous = 0;
+    if (!options) options = {};
+    const later = function() {
+      previous = options.leading === false ? 0 : new Date();
+      timeout = null;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    };
 
-		return function(event) {
-			const now = new Date();
-			if (!previous && options.leading === false) previous = now;
-			const remaining = wait - (now - previous);
-			context = this;
-			args = arguments;
-			if (remaining <= 0 || remaining > wait) {
-				if (timeout) {
-					clearTimeout(timeout);
-					timeout = null;
-				}
-				previous = now;
-				result = func.apply(context, args);
-				if (!timeout) context = args = null;
-			} else if (!timeout && options.trailing !== false) {
-				timeout = setTimeout(later, remaining);
-			}
-			return result;
-		};
-	};
+    return function(event) {
+      const now = new Date();
+      if (!previous && options.leading === false) previous = now;
+      const remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0 || remaining > wait) {
+        if (timeout) {
+          clearTimeout(timeout);
+          timeout = null;
+        }
+        previous = now;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
+    };
+  };
 </script>
